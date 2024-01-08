d5eb9b8096a1bc0e5ea1d33b1280eb14b9a6b9a3
ZAIN ALI <97796695+mrZAIN-ALI@users.noreply.github.com>
2024-01-08 16:50:00 +0500

    empty

---
+++ lib/deserves.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class CounterController extends GetxController {
+  var counter = 0.obs;
+
+  void increment() {
+    counter++;
+  }
+
+  void decrement() {
+    counter--;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('GetX Counter App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GetBuilder<CounterController>(
+                init: CounterController(),
+                builder: (controller) {
+                  return Text(
+                    'Count: ${controller.counter}',
+                    style: TextStyle(fontSize: 24),
+                  );
+                },
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Get.find<CounterController>().increment();
+                    },
+                    child: Text('Increment'),
+                  ),
+                  SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      Get.find<CounterController>().decrement();
+                    },
+                    child: Text('Decrement'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+//why so lazy
